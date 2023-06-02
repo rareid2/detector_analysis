b6d7bc4c62ee01eaa367afbfad5dcb48e0ddbfd9
Riley Anne Reid <rileyannereid@lair12.int.colorado.edu>
2023-06-02 13:14:13 -0600

    final run for no aperture iso

--- run.py
+++ run.py
@@ -38,7 +38,6 @@ n_particles = 1e8
 """
 rank = 1
 element_size = 1.76/2 # mm
-pixels_downsample = 16 #int(10 * det_size_cm / (2 * element_size))
 n_elements = rank
 mask_size = det_size_cm * 10 # convert to mm
 trim = None
@@ -51,13 +50,11 @@ thickness = 500  # um
 
 # focal length
 distance = 2.4  # cm
-
-# number of particles to simulate
-n_particles = 1e9
 """
 # --------------set up simulation---------------
 signals = []
-for i in range(3):
+for radius in range(3):
+    # for n_particles in [1e5,1e6,1e7,1e8,1e9]:
     simulation_engine.set_config(
         det1_thickness_um=300,
         det_gap_mm=30,  # gap between first and second (unused detector)
@@ -76,20 +73,21 @@ for i in range(3):
     energy_level = 500  # keV
 
     # --------------set up data naming---------------
-    fname_tag = f"sphere-no-aperture-{i}"
+    fname_tag = f"sphere-no-aperture-{radius}"
+    # fname_tag = f"sphere-no-aperture"
     fname = f"../simulation-data/{fname_tag}_{n_particles:.2E}_{energy_type}_{energy_level}.csv"
 
     simulation_engine.set_macro(
         n_particles=n_particles,
         energy_keV=[energy_type, energy_level, None],
         sphere=True,
-        radius_cm=4,
+        radius_cm=2,
         progress_mod=int(n_particles / 10),  # set with 10 steps
         fname_tag=fname_tag,
     )
 
     # --------------RUN---------------
-    simulation_engine.run_simulation(fname, build=True)
+    # simulation_engine.run_simulation(fname, build=True)
 
     # ---------- process results -----------
     myhits = Hits(fname=fname, experiment=False)
@@ -101,13 +99,13 @@ for i in range(3):
     _, _, heatmap, _ = deconvolver.deconvolve(
         plot_deconvolved_heatmap=False,
         plot_raw_heatmap=True,
-        save_raw_heatmap=f"../simulation-results/{fname_tag}_{n_particles:.2E}_{energy_type}_{energy_level}_raw.png",
-        save_deconvolve_heatmap=f"../simulation-results/{fname_tag}_{n_particles:.2E}_{energy_type}_{energy_level}_dc.png",
+        save_raw_heatmap=f"../simulation-results/validating-iso/{fname_tag}_{n_particles:.2E}_{energy_type}_{energy_level}_raw.png",
+        save_deconvolve_heatmap=f"../simulation-results/validating-iso/{fname_tag}_{n_particles:.2E}_{energy_type}_{energy_level}_dc.png",
         downsample=1,
         trim=trim,
         plot_signal_peak=True,
         plot_conditions=False,
-        save_peak=f"../simulation-results/{fname_tag}_{n_particles:.2E}_{energy_type}_{energy_level}_peak.png",
+        save_peak=f"../simulation-results/validating-iso/{fname_tag}_{n_particles:.2E}_{energy_type}_{energy_level}_peak.png",
     )
 
     signal = np.sum(heatmap, axis=1)
@@ -119,15 +117,17 @@ import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots(1, 1)
 
-for signal in signals:
-    ax.plot(signal)
+colors = ["#BBDEF0", "#EFCA08", "#F08700"]
+for signal, color in zip(signals, colors):
+    ax.plot(signal, c=color)
     avg = np.average(signal)
     noise = np.sqrt(signal)
     avg_noise = np.average(noise)
 
     print(avg, avg_noise)
-    ax.hlines(y=avg, xmin=0, xmax=242, linewidth=2, color="r")
-    plt.hlines(y=avg - noise, xmin=0, xmax=242, linewidth=2, color="r")
-    plt.hlines(y=avg + noise, xmin=0, xmax=242, linewidth=2, color="r")
-
-plt.savefig("no-aperture-no-cap.png")
+ax.hlines(y=avg, xmin=0, xmax=len(signal), linewidth=2, color="#00A6A6")
+plt.hlines(y=avg - noise, xmin=0, xmax=len(signal), linewidth=2, color="#00A6A6")
+plt.hlines(y=avg + noise, xmin=0, xmax=len(signal), linewidth=2, color="#00A6A6")
+plt.xlabel("pixel #")
+plt.ylabel("# hits")
+plt.savefig("../simulation-results/validating-iso/no-aperture-cap-final.png", dpi=300)
