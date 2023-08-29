7a4e60516cccd7f8607a395da50ca8e0a03b2e2f
Riley Anne Reid <rileyannereid@lair12.int.colorado.edu>
2023-08-29 12:18:42 -0600

    added vertex hits processing and a func to get fcfov;

--- hits.py
+++ hits.py
@@ -1,4 +1,4 @@
-import os, re
+import os, math
 import pandas as pd
 import numpy as np
 from experiment_engine import ExperimentEngine
@@ -88,7 +88,7 @@ class Hits:
         else:
             self.detector_hits = pd.read_csv(
                 self.fname,
-                names=["det", "x", "y", "z", "energy", "ID", "name"],
+                names=["det", "x", "y", "z", "energy", "ID", "name", "x0", "y0", "z0"],
                 dtype={
                     "det": np.int8,
                     "x": np.float64,
@@ -97,6 +97,9 @@ class Hits:
                     "energy": np.float64,
                     "ID": np.int8,
                     "name": str,
+                    "x0": np.float64,
+                    "y0": np.float64,
+                    "z0": np.float64,
                 },
                 delimiter=",",
                 on_bad_lines="skip",
@@ -191,7 +194,10 @@ class Hits:
 
     # get hits for generalized setup in geant4
     def get_det_hits(
-        self, remove_secondaries: bool = False, second_axis: str = "y"
+        self,
+        remove_secondaries: bool = False,
+        second_axis: str = "y",
+        energy_level: float = 500,
     ) -> dict:
         """
         return a dictionary containing hits on front detector
@@ -204,26 +210,49 @@ class Hits:
         posX = []
         posY = []
         energies = []
-        detector_offset = 1111 * 0.45 - (0.03 / 2) # TODO: make this dynamic
+        vertex = []
+        detector_offset = 1111 * 0.45 - (0.03 / 2)  # TODO: make this dynamic
         for count, el in enumerate(self.detector_hits["det"]):
             # only get hits on the first detector
             if el == 1 and remove_secondaries != True:
                 xpos = self.detector_hits["x"][count]
                 ypos = self.detector_hits["y"][count]
                 zpos = self.detector_hits["z"][count]
+                energy_keV = self.detector_hits["energy"][count]
 
-                if second_axis == "z" and ypos == 0.015:
+                if second_axis == "z" and ypos == 0.015 and energy_keV == energy_level:
                     posX.append(xpos)
                     posY.append(zpos - detector_offset)
-                elif second_axis == "y" and zpos == detector_offset:
+                    energies.append(energy_keV)
+                    vertex.append(
+                        np.array(
+                            [
+                                self.detector_hits["x0"][count],
+                                self.detector_hits["y0"][count],
+                                self.detector_hits["z0"][count],
+                            ]
+                        )
+                    )
+                elif (
+                    second_axis == "y"
+                    and zpos == detector_offset
+                    and energy_keV == energy_level
+                ):
                     posX.append(xpos)
                     posY.append(ypos)
+                    energies.append(energy_keV)
+                    vertex.append(
+                        np.array(
+                            [
+                                self.detector_hits["x0"][count],
+                                self.detector_hits["y0"][count],
+                                self.detector_hits["z0"][count],
+                            ]
+                        )
+                    )
                 else:
-                    pass 
-                
-                # get energy
-                energy_kev = self.detector_hits["energy"][count]
-                energies.append(energy_kev)
+                    pass
+
             # if checking for secondaries and want to remove them, only process electrons
             elif el == 1 and remove_secondaries:
                 if (
@@ -233,25 +262,50 @@ class Hits:
                     xpos = self.detector_hits["x"][count]
                     ypos = self.detector_hits["y"][count]
                     zpos = self.detector_hits["z"][count]
+                    energy_keV = self.detector_hits["energy"][count]
 
-                    if second_axis == "z" and ypos == 0.015:
+                    if (
+                        second_axis == "z"
+                        and ypos == 0.015
+                        and energy_keV == energy_level
+                    ):
                         posX.append(xpos)
                         posY.append(zpos - detector_offset)
-                    elif second_axis == "y" and zpos == detector_offset:
+                        energies.append(energy_keV)
+                        vertex.append(
+                            np.array(
+                                [
+                                    self.detector_hits["x0"][count],
+                                    self.detector_hits["y0"][count],
+                                    self.detector_hits["z0"][count],
+                                ]
+                            )
+                        )
+                    elif (
+                        second_axis == "y"
+                        and zpos == detector_offset
+                        and energy_keV == energy_level
+                    ):
                         posX.append(xpos)
                         posY.append(ypos)
+                        energies.append(energy_keV)
+                        vertex.append(
+                            np.array(
+                                [
+                                    self.detector_hits["x0"][count],
+                                    self.detector_hits["y0"][count],
+                                    self.detector_hits["z0"][count],
+                                ]
+                            )
+                        )
                     else:
-                        pass 
-                    
-                    # get energy
-                    energy_kev = self.detector_hits["energy"][count]
-                    energies.append(energy_kev)
+                        pass
             else:
                 pass
 
         hits_pos = [(X, Y) for X, Y in zip(posX, posY)]
 
-        hits_dict = {"Position": hits_pos, "Energy": energies}
+        hits_dict = {"Position": hits_pos, "Energy": energies, "Vertices": vertex}
 
         self.hits_dict = hits_dict
 
@@ -335,4 +389,164 @@ class Hits:
             # unpdate entry
             self.hits_dict["Position"] = unc_pos
 
+        return
+
+    def exclude_pcfov(
+        self, detector_dim, mask_dim, focal_length, sphere_radius, second_axis
+    ):
+        # create locations for sensor corners and mask corners
+
+        if second_axis == "z":
+            pixel0 = np.array([detector_dim / 2, -1 * focal_length, detector_dim / 2])
+            mask0 = np.array([mask_dim / 2, 0, mask_dim / 2])
+
+            pixel1 = np.array(
+                [-1 * detector_dim / 2, -1 * focal_length, detector_dim / 2]
+            )
+            mask1 = np.array([-1 * mask_dim / 2, 0, mask_dim / 2])
+
+            pixel2 = np.array(
+                [detector_dim / 2, -1 * focal_length, -1 * detector_dim / 2]
+            )
+            mask2 = np.array([mask_dim / 2, 0, -1 * mask_dim / 2])
+
+            pixel3 = np.array(
+                [-1 * detector_dim / 2, -1 * focal_length, -1 * detector_dim / 2]
+            )
+            mask3 = np.array([-1 * mask_dim / 2, 0, -1 * mask_dim / 2])
+        else:
+            pixel0 = np.array(
+                [
+                    detector_dim / 2,
+                    detector_dim / 2,
+                    -1 * focal_length,
+                ]
+            )
+            mask0 = np.array([mask_dim / 2, mask_dim / 2, 0])
+
+            pixel1 = np.array(
+                [-1 * detector_dim / 2, detector_dim / 2, -1 * focal_length]
+            )
+            mask1 = np.array([-1 * mask_dim / 2, mask_dim / 2, 0])
+
+            pixel2 = np.array(
+                [detector_dim / 2, -1 * detector_dim / 2, -1 * focal_length]
+            )
+            mask2 = np.array([mask_dim / 2, -1 * mask_dim / 2, 0])
+
+            pixel3 = np.array(
+                [-1 * detector_dim / 2, -1 * detector_dim / 2, -1 * focal_length]
+            )
+            mask3 = np.array([-1 * mask_dim / 2, -1 * mask_dim / 2, 0])
+        # Calculate the vector from point1 to point2
+        vector0 = pixel0 - mask0
+        vector1 = pixel1 - mask1
+        vector2 = pixel2 - mask2
+        vector3 = pixel3 - mask3
+
+        ray_directions = [vector0, vector1, vector2, vector3]
+
+        def ray_sphere_intersection(
+            ray_origin, ray_direction, sphere_center, sphere_radius
+        ):
+            # Calculate the vector from the ray's origin to the sphere's center
+            oc = ray_origin - sphere_center
+
+            # Calculate coefficients for the quadratic equation
+            a = np.dot(ray_direction, ray_direction)
+            b = 2.0 * np.dot(oc, ray_direction)
+            c = np.dot(oc, oc) - sphere_radius**2
+
+            # Calculate the discriminant
+            discriminant = b**2 - 4 * a * c
+
+            if discriminant < 0:
+                return None  # No intersection
+
+            # Calculate the two solutions for t (parameter along the ray)
+            t1 = (-b - np.sqrt(discriminant)) / (2.0 * a)
+            t2 = (-b + np.sqrt(discriminant)) / (2.0 * a)
+
+            # Choose the smaller positive t value as the intersection point
+            t = min(t1, t2)
+
+            # Calculate the intersection point
+            intersection_point = ray_origin + t * ray_direction
+
+            return intersection_point
+
+        # Define the ray's origin and direction
+        pixels = [pixel0, pixel1, pixel2, pixel3]
+        vectors = [vector0, vector1, vector2, vector3]
+
+        intersection_points = []
+        for vector in vectors:
+            # ray origin is from sphere center
+            ray_origin = np.array([0, 0, 0])
+            vector /= np.linalg.norm(vector)
+            ray_direction = vector
+
+            # Define the sphere's center and radius
+            sphere_center = np.array([0.0, 0.0, 0.0])
+
+            # Calculate the intersection point on the surface of the sphere
+            intersection_point = ray_sphere_intersection(
+                ray_origin, ray_direction, sphere_center, sphere_radius
+            )
+            intersection_points.append(intersection_point)
+
+        # find the theta and phi extent from the intersection points
+        theta_extents = []
+        phi_extents = []
+
+        for itp in intersection_points:
+            x = itp[0]
+            y = itp[1]
+            z = itp[2]
+
+            r = math.sqrt(x**2 + y**2 + z**2)
+            phi = math.atan2(y, x)
+            theta = math.acos(z / r)
+
+            theta_extents.append(theta)
+            phi_extents.append(phi)
+
+        # unpack into extents
+        theta_min = min(np.array(theta_extents))
+        theta_max = max(np.array(theta_extents))
+        phi_min = min(np.array(phi_extents))
+        phi_max = max(np.array(phi_extents))
+
+        inside_idx = []
+        # finally, go through hits and determine if within extents
+        for vi, vtx in enumerate(self.hits_dict["Vertices"]):
+            # adjust for sphere center - TODO fix this to be dynamic!
+            xc = 0
+            yc = 0
+            zc = 497.915
+            x = vtx[0] - xc
+            y = vtx[1] - yc
+            z = vtx[2] - zc
+
+            # calculate in spherical
+            r = math.sqrt(x**2 + y**2 + z**2)
+            phi = math.atan2(y, x)
+            theta = math.acos(z / r)
+
+            if second_axis == "z":
+                if theta < theta_max:
+                    inside_idx.append(vi)
+                else:
+                    pass
+            else:
+                if theta_min < theta < theta_max and phi_min < phi < phi_max:
+                    inside_idx.append(vi)
+                else:
+                    pass
+        inside_hits = {}
+        # save only those inside
+        for key, array in self.hits_dict.items():
+            inside_hits[key] = [array[i] for i in inside_idx]
+
+        self.hits_dict = inside_hits
         return
