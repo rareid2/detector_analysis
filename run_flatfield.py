409fb29e083eccc6f27399be9dac554000593cd5
Riley Anne Reid <rileyannereid@lair12.int.colorado.edu>
2023-09-26 12:10:17 -0600

    updated to run hits, no optimization

--- run_flatfield.py
+++ run_flatfield.py
@@ -11,14 +11,16 @@ import os
 # construct = CA and TD
 # source = DS and PS
 
+# need to run for each indices in each direction, 3 times
 
-def run_flat(fudge, inc, dir, i, cone=False, vmax=None):
-    fudge = fudge[0]
+
+def run_geom_corr(ind, direction, i, vmax=None):
     simulation_engine = SimulationEngine(construct="CA", source="PS", write_files=True)
 
     # general detector design
     det_size_cm = 3.05  # cm
     pixel = 0.5  # mm
+    pixel_cm = 0.05
 
     # ---------- coded aperture set up ---------
 
@@ -35,6 +37,7 @@ def run_flat(fudge, inc, dir, i, cone=False, vmax=None):
     mosaic = True
 
     # thickness of mask
+    det_thickness = 300  # um
     thickness = 400  # um
 
     # focal length
@@ -43,16 +46,45 @@ def run_flat(fudge, inc, dir, i, cone=False, vmax=None):
     fake_radius = 1
 
     # set distance of the source to the detector
-    src_plane_distance = (1111 * 0.45) + 500
-    if dir == "xy":
-        theta = np.arctan(
-            (pixel * inc * fudge * np.sqrt(2)) * 0.1 / distance
-        )  # np.sqrt(2) - add sqrt(2) for diagonal
+    world_offset = 1111 * 0.45
+    detector_loc = world_offset - ((det_thickness / 2) * 1e-4)
+    ca_pos = world_offset - distance - (((thickness / 2) + (det_thickness / 2)) * 1e-4)
+    src_plane_distance = world_offset + 500
+
+    # pixel location
+    pix_ind = ind
+    if direction == "x":
+        px = pixel_cm * pix_ind
+        py = 0
+    elif direction == "y":
+        px = 0
+        py = pixel_cm * pix_ind
+    elif direction == "xy":
+        px = pixel_cm * pix_ind
+        py = pixel_cm * pix_ind
     else:
-        theta = np.arctan((pixel * inc * fudge) * 0.1 / distance)
-    src_pos_y = np.tan(theta) * src_plane_distance
-    new_x = src_pos_y * np.cos(np.deg2rad(45))
-    new_y = src_pos_y * np.sin(np.deg2rad(45))
+        px = 0
+        py = 0
+    pz = detector_loc
+
+    # source plane
+    src_plane_normal = np.array([0, 0, 1])  # Normal vector of the plane
+    src_plane_pt = np.array([0, 0, -500])  # A point on the plane
+
+    px_point = np.array([px, py, pz])  # A point on the line
+    px_ray = np.array([0 - px, 0 - py, ca_pos - pz])
+
+    ndotu = src_plane_normal.dot(px_ray)
+
+    epsilon = 1e-8
+    if abs(ndotu) < epsilon:
+        print("no intersection or line is within plane")
+
+    w = px_point - src_plane_pt
+    si = -src_plane_normal.dot(w) / ndotu
+    src_point = w + si * px_ray + src_plane_pt
+
+    # now we have location of the src_point
 
     # ------------------- simulation parameters ------------------
     n_particles = 1e6
@@ -77,93 +109,24 @@ def run_flat(fudge, inc, dir, i, cone=False, vmax=None):
     energy_level = 100  # keV
 
     # --------------set up data naming---------------
-    formatted_theta = "{:.0f}p{:03d}".format(int(theta), int((theta % 1) * 100))
-    if dir != "0":
-        fname_tag = (
-            f"{n_elements_original}-{distance}-{formatted_theta}-{dir}-f-{i}-ptsrc"
-        )
-    else:
-        fname_tag = f"{n_elements_original}-{distance}-{formatted_theta}-f-{i}-ptsrc"
+    fname_tag = (
+        f"{n_elements_original}-{distance}-{ind}-{direction}-geom-corr-{i}-ptsrc"
+    )
     fname = f"../simulation-data/aperture-collimation/{fname_tag}_{n_particles:.2E}_{energy_type}_{energy_level}.csv"
 
-    # set angular extent of the cone pointing on axis
-    maxtheta = np.rad2deg(
-        np.arctan((det_size_cm * np.sqrt(2) / 2) / src_plane_distance)
+    simulation_engine.set_macro(
+        n_particles=int(n_particles),
+        energy_keV=[energy_level],
+        surface=False,
+        positions=[[src_point[0], src_point[1], -500]],
+        directions=[1],
+        progress_mod=int(n_particles / 10),  # set with 10 steps
+        fname_tag=fname_tag,
+        detector_dim=det_size_cm,
     )
 
-    if dir == "xy":
-        newpos = np.sqrt(new_x**2 + new_y**2)
-        src_det_dist = np.sqrt(newpos**2 + src_plane_distance**2)
-        alpha = np.arctan((pixel * inc * fudge) * 0.1 / distance)
-    else:
-        src_det_dist = np.sqrt(src_pos_y**2 + src_plane_distance**2)
-        alpha = theta
-
-    omega = maxtheta
-    p1 = (
-        src_det_dist
-        * np.tan(np.deg2rad(omega))
-        * np.cos(np.deg2rad(omega))
-        / np.cos(np.deg2rad(alpha + omega))
-    )
-    p2 = (
-        src_det_dist
-        * np.tan(np.deg2rad(omega))
-        * np.cos(np.deg2rad(omega))
-        / np.cos(np.deg2rad(alpha - omega))
-    )
-
-    a = (p1 + p2) / 2
-    b = src_det_dist * np.tan(np.deg2rad(omega)) * a / (np.sqrt(p1 * p2))
-
-    # calculate the cone area
-    cone_area = np.pi * a * b
-    if dir == "xy":
-        simulation_engine.set_macro(
-            n_particles=int(n_particles),
-            energy_keV=[energy_level],
-            surface=False,
-            positions=[[new_x, new_y, -500]],
-            directions=[1],
-            progress_mod=int(n_particles / 10),  # set with 10 steps
-            fname_tag=fname_tag,
-            detector_dim=det_size_cm,
-        )
-    elif dir == "x":
-        simulation_engine.set_macro(
-            n_particles=int(n_particles),
-            energy_keV=[energy_level],
-            surface=False,
-            positions=[[src_pos_y, 0, -500]],
-            directions=[1],
-            progress_mod=int(n_particles / 10),  # set with 10 steps
-            fname_tag=fname_tag,
-            detector_dim=det_size_cm,
-        )
-    elif dir == "y":
-        simulation_engine.set_macro(
-            n_particles=int(n_particles),
-            energy_keV=[energy_level],
-            surface=False,
-            positions=[[0, src_pos_y, -500]],
-            directions=[1],
-            progress_mod=int(n_particles / 10),  # set with 10 steps
-            fname_tag=fname_tag,
-            detector_dim=det_size_cm,
-        )
-    else:
-        simulation_engine.set_macro(
-            n_particles=int(n_particles),
-            energy_keV=[energy_level],
-            surface=False,
-            positions=[[0, 0, -500]],
-            directions=[1],
-            progress_mod=int(n_particles / 10),  # set with 10 steps
-            fname_tag=fname_tag,
-            detector_dim=det_size_cm,
-        )
     # --------------RUN---------------
-    simulation_engine.run_simulation(fname, build=False, rename=True)
+    # simulation_engine.run_simulation(fname, build=False, rename=True)
 
     # ---------- process results -----------
 
@@ -171,12 +134,12 @@ def run_flat(fudge, inc, dir, i, cone=False, vmax=None):
     myhits.get_det_hits(
         remove_secondaries=True, second_axis="y", energy_level=energy_level
     )
-
+    # print(len(myhits.hits_dict["Position"]))
     # deconvolution steps
     deconvolver = Deconvolution(myhits, simulation_engine)
 
     # directory to save results in
-    results_dir = "../simulation-results/aperture-collimation/with-correction/"
+    results_dir = "../simulation-results/aperture-collimation/geom-correction/"
     results_tag = f"{fname_tag}_{n_particles:.2E}_{energy_type}_{energy_level}"
     results_save = results_dir + results_tag
 
@@ -191,244 +154,108 @@ def run_flat(fudge, inc, dir, i, cone=False, vmax=None):
         plot_signal_peak=False,
         plot_conditions=False,
     )
-    inds = (32 + 2 * ((inc / 2) - 1), 28 - 2 * ((inc / 2) - 1))
-    print("checked index #", inds)
 
-    if cone:
-        if dir == "x":
-            return deconvolver.deconvolved_image[int(inds[0]), 30], cone_area
-        elif dir == "y":
-            return deconvolver.deconvolved_image[30, int(inds[1])], cone_area
-        elif dir == "xy":
-            return deconvolver.deconvolved_image[int(inds[0]), int(inds[1])], cone_area
-        else:
-            return deconvolver.deconvolved_image[30, 30], cone_area
+    # return len(myhits.hits_dict["Position"])
+    # Find the indices of the maximum value in the flattened array
+    max_index_flat = np.argmax(deconvolver.deconvolved_image)
+
+    # Convert the flattened index to 2D indices
+    max_index_2d = np.unravel_index(max_index_flat, deconvolver.deconvolved_image.shape)
+
+    # Print the result
+    inds = (32 + 2 * ((ind / 2) - 1), 28 - 2 * ((ind / 2) - 1))
+    print("Indices of the maximum value (2D):", max_index_2d)
+
+    if direction == "x":
+        print(max_index_2d, int(inds[1]), 30)
+        return deconvolver.deconvolved_image[int(inds[1]), 30]
+    elif direction == "y":
+        return deconvolver.deconvolved_image[30, int(inds[0])]
+    elif direction == "xy":
+        return deconvolver.deconvolved_image[int(inds[1]), int(inds[0])]
     else:
-        if dir == "x":
-            return deconvolver.deconvolved_image[int(inds[0]), 30]
-        elif dir == "y":
-            return deconvolver.deconvolved_image[30, int(inds[1])]
-        elif dir == "xy":
-            return deconvolver.deconvolved_image[int(inds[0]), int(inds[1])]
-        else:
-            return deconvolver.deconvolved_image[30, 30]
+        return deconvolver.deconvolved_image[30, 30]
 
 
-# --- --- --- --- optimization to get the pixel correct --- --- --- ---
-dec, center_cone = run_flat([0.99], 28, "x", 0, cone=True, vmax=None)
-"""
-initial_guess = 0.99
-bounds = [(0.97, 1.02)]
-
+# setup
+data_folder = "/home/rileyannereid/workspace/geant4/simulation-results/aperture-collimation/geom-correction/"
 maxpixel = 30
 pix_int = 2
 incs = range(pix_int, maxpixel + pix_int, pix_int)
-data_folder = "/home/rileyannereid/workspace/geant4/simulation-results/aperture-collimation/with-correction/"
+niter = 5
 
-optimization_file = "-f.txt"
-final_opts = []
-for dir in ["x", "y", "xy"]:
-    optimizations = []
-    # Use the minimize function with the negative objective to find the maximum
+
+# RUN GEOMETRY NORMALIZATION ------------------ normalize total number of possible hits on detector
+# run center hits first
+
+avg_hits = 0
+for i in range(niter):
+    nhits = run_geom_corr(0, "0", i)
+    avg_hits += nhits
+
+# save it
+center_hits = avg_hits / niter
+np.savetxt(
+    f"{data_folder}center-hits-norm.txt",
+    np.array([center_hits]),
+    delimiter=", ",
+    fmt="%.14f",
+)
+
+# run each direction hits - no mask
+all_hits = []
+for direction in ["x", "y", "xy"]:
+    total_hits = []
     for inc in incs:
-        result = minimize(
-            lambda x: -run_flat(x, inc, dir, 0),
-            initial_guess,
-            bounds=bounds,
-            options={"maxiter": 15},
-        )
-
-        # Print the optimization result
-        if result.success:
-            print("Optimal solution found:")
-            print("x =", result.x)
-            print("Maximum value =", -result.fun)
-        else:
-            print("Optimization failed:", result.message)
-
-        optimizations.append(result.x)
-    optimizations_array = np.array(optimizations)
+        avg_hits = 0
+        for i in range(niter):
+            nhits = run_geom_corr(inc, direction, i)
+            avg_hits += nhits
+        total_hits.append((avg_hits / niter) / center_hits)
     np.savetxt(
-        f"{data_folder}{dir}{optimization_file}",
-        optimizations_array,
+        f"{data_folder}{direction}-hits-norm.txt",
+        np.array(total_hits),
         delimiter=", ",
         fmt="%.14f",
     )
-    final_opts.append(optimizations)
+    all_hits.append(total_hits)
 
-# --- --- --- --- now run 3 at each to get average signal and size of cone --- --- --- ---
+print(all_hits)
 
-signal_strengths = []
-for i in range(3):
-    dec, center_cone = run_flat([1], 0, "0", i, cone=True, vmax=None)
-    signal_strengths.append(dec)
+# RUN SIGNAL NORMALIZATION ------------------ normalize total number of possible hits on detector
+# run center hits first
 
-signal_norm = np.mean(np.array(signal_strengths))
-cone_norm = center_cone
+avg_signal = 0
+for i in range(niter):
+    dec = run_geom_corr(0, "0", i)
+    print(dec)
+    avg_signal += dec
 
-all_cones = []
-all_signals = []
-for dir, ff in zip(dirs, final_opts):
-    cones = []
-    signals = []
-    for inc, f in zip(incs, ff):
-        signal_avg = 0
-        for i in range(3):
-            dec, cone = run_flat([f], inc, dir, i, cone=True, vmax=signal_norm)
-            signal_avg += dec
-        # save the average for the pixel and the cone size
-        signals.append(signal_avg / 3)
-        cones.append(cone)
+# save it
+sig_norm = avg_signal / niter
+np.savetxt(
+    f"{data_folder}center-sig-norm.txt",
+    np.array([sig_norm]),
+    delimiter=", ",
+    fmt="%.14f",
+)
+
+# run each direction hits - no mask
+all_hits = []
+for direction in ["x", "y", "xy"]:
+    total_hits = []
+    for inc in incs:
+        avg_hits = 0
+        for i in range(niter):
+            nhits = run_geom_corr(inc, direction, i, vmax=sig_norm)
+            avg_hits += nhits
+        total_hits.append((avg_hits / niter) / sig_norm)
     np.savetxt(
-        f"{data_folder}{dir}-cone.txt", np.array(cones), delimiter=", ", fmt="%.14f"
+        f"{data_folder}{direction}-sig.txt",
+        np.array(total_hits),
+        delimiter=", ",
+        fmt="%.14f",
     )
-    np.savetxt(
-        f"{data_folder}{dir}-signal.txt", np.array(signals), delimiter=", ", fmt="%.14f"
-    )
-    all_cones.append(cones)
-    all_signals.append(signals)
+    all_hits.append(total_hits)
 
-# --- --- --- --- get the final normalization --- --- --- ---
-
-x_signals_avg = [
-    (all_cones[0][i] / cone_norm) * xs / signal_norm
-    for i, xs in enumerate(all_signals[0])
-]
-y_signals_avg = [
-    (all_cones[1][i] / cone_norm) * ys / signal_norm
-    for i, ys in enumerate(all_signals[1])
-]
-xy_signals_avg = [
-    (all_cones[2][i] / cone_norm) * xys / signal_norm
-    for i, xys in enumerate(all_signals[2])
-]
-
-# save the final data
-data_2dx = np.array(x_signals_avg)
-data_2dy = np.array(y_signals_avg)
-data_2dxy = np.array(xy_signals_avg)
-
-# Define the output file path
-output_filex = "x-signals-norm.txt"
-output_filey = "y-signals-norm.txt"
-output_filexy = "xy-signals-norm.txt"
-
-# Save the 2D array to the text file
-np.savetxt(f"{data_folder}{output_filex}", data_2dx, delimiter=", ", fmt="%.14f")
-np.savetxt(f"{data_folder}{output_filey}", data_2dy, delimiter=", ", fmt="%.14f")
-np.savetxt(f"{data_folder}{output_filexy}", data_2dxy, delimiter=", ", fmt="%.14f")
-
-"""
-"""
-
-# save data here just in case
-# x
-fx = [
-    0.98997616,
-    0.98999416,
-    0.99,
-    0.98999924,
-    0.99000025,
-    0.99,
-    0.98999993,
-    0.99000094,
-    0.99,
-    0.98999835,
-    0.99001095,
-    0.98999702,
-    0.99,
-    0.99000219,
-    1.01,
-]
-# y
-fy = [
-    0.99000002,
-    0.99000079,
-    0.99000412,
-    0.99002364,
-    0.99,
-    0.99001873,
-    0.98999748,
-    0.98998595,
-    0.98999644,
-    0.99,
-    0.99006229,
-    0.98998045,
-    0.99,
-    0.99000011,
-    1.01,
-]
-
-# diagonal
-fd = [
-    0.99,
-    0.99000611,
-    0.99003122,
-    0.99,
-    0.98998818,
-    0.98999197,
-    0.98999184,
-    0.9899989,
-    0.98999992,
-    0.99000783,
-    0.99000049,
-    0.98999899,
-    0.98999891,
-    0.98999995,
-    1.01,
-]
-
-x_signals = [
-    [289116.00000000006, 289358.00000000006, 289460.00000000006],
-    [284610.00000000006, 285682.00000000006, 285732.00000000006],
-    [280024.0000000001, 279634.00000000006, 278946.00000000006],
-    [271602.0000000001, 271114.0000000001, 271292.00000000006],
-    [264224.0, 263176.00000000006, 263554.0000000001],
-    [254808.00000000012, 254506.0000000001, 254746.00000000006],
-    [244302.0000000001, 244946.0000000001, 244774.0000000001],
-    [234392.00000000003, 233536.00000000006, 235416.00000000006],
-    [225632.00000000006, 225282.00000000006, 225856.00000000003],
-    [215864.00000000006, 215272.00000000006, 216060.00000000006],
-    [206312.00000000006, 207862.00000000006, 207588.00000000006],
-    [197122.00000000003, 197057.99999999997, 196860.00000000006],
-    [187424.00000000003, 186920.00000000006, 186916.00000000003],
-    [178828.00000000003, 178388.00000000003, 178124.00000000006],
-    [107130.00000000003, 106680.00000000004, 106810.00000000001],
-]
-
-y_signals = [
-    [290196.00000000006, 289078.00000000006, 289134.0000000001],
-    [286094.00000000006, 285752.00000000006, 285774.00000000006],
-    [279972.00000000006, 280502.00000000006, 280132.00000000006],
-    [272890.0000000001, 272826.0000000001, 272858.00000000006],
-    [265050.00000000006, 263716.0000000001, 265190.00000000006],
-    [256032.0000000001, 255692.0000000001, 256608.0000000001],
-    [249042.00000000012, 249542.0000000001, 250552.0000000001],
-    [236664.00000000003, 236490.00000000006, 237048.0000000001],
-    [227654.00000000006, 228698.00000000006, 227798.00000000006],
-    [218090.00000000006, 219030.00000000006, 218706.00000000003],
-    [214410.00000000006, 215014.00000000006, 214290.00000000003],
-    [201162.00000000006, 201208.00000000006, 202020.00000000006],
-    [191968.00000000006, 190450.00000000006, 191778.00000000006],
-    [188834.00000000006, 189064.00000000003, 189310.00000000006],
-    [112200.0, 113010.0, 112206.00000000001],
-]
-xy_signals = [
-    [287428.00000000006, 287282.0000000001, 286840.0000000001],
-    [281876.00000000006, 280964.00000000006, 281288.00000000006],
-    [270366.00000000006, 270110.00000000006, 269608.0000000001],
-    [254594.00000000003, 254126.0000000001, 254658.0000000001],
-    [240024.0000000001, 239494.0000000001, 239386.0000000001],
-    [224836.00000000003, 223214.00000000015, 223922.00000000006],
-    [210392.00000000006, 209314.00000000006, 209876.00000000006],
-    [192120.00000000006, 191354.00000000003, 191748.0],
-    [177412.00000000003, 177176.00000000006, 177030.00000000003],
-    [164240.00000000006, 164140.00000000006, 163884.00000000003],
-    [154560.00000000006, 153806.00000000003, 154070.0],
-    [138372.00000000006, 138682.00000000003, 138352.00000000006],
-    [126500.00000000004, 125916.00000000004, 126512.00000000004],
-    [120728.00000000003, 120428.00000000003, 120982.00000000001],
-    [46852.00000000001, 45992.00000000001, 46000.0],
-]
-
-"""
+print(all_hits)
